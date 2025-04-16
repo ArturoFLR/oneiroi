5fa83fe745ee5f5ab81839f606b8fef1a4311ef6
Arturo López <jhonas29@gmail.com>
2025-04-16 11:30:08 +0200

    chore(sound): Initialize singleton instances for sound manager classes in singletons.ts

---
+++ src/classes/sound/singletons.ts
@@ -0,0 +1,13 @@
+import SoundDirectorAPI from "./SoundDirectorAPI";
+import SoundEffects from "./SoundEffects";
+import SoundParameterController from "./SoundParameterController";
+import SoundPlayer from "./SoundPlayer";
+import SoundscapesCreator from "./SoundscapesCreator";
+import SoundStore from "./SoundStore";
+
+export const SoundDirectorAPI1 = SoundDirectorAPI.getInstance();
+export const SoundStore1 = SoundStore.getInstance();
+export const SoundPlayer1 = SoundPlayer.getInstance();
+export const SoundParameterController1 = SoundParameterController.getInstance();
+export const SoundEffects1 = SoundEffects.getInstance();
+export const SoundscapesCreator1 = SoundscapesCreator.getInstance();
