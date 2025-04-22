dc6977d3337336301be4c1cf0cf9b8127bc3de71
Arturo López <jhonas29@gmail.com>
2025-04-22 19:13:42 +0200

    refactor(utils): Add a minimum font size to ensure readability.

--- src/utils/calcFontSize.ts
+++ src/utils/calcFontSize.ts
@@ -1,16 +1,18 @@
 //Esta función calcula el tamaño de fuente apropiada según el ancho del contenedor, que habremos guardado con useRef() y pasamos como argumento.
 //Cuanto mayor sea textProportion, menor será el tamaño de la fuente. 8 es un valor que da buenos resultados en general.
+// Nunca devuelve un tamaño inferior a 12px, por seguridad y legibiilidad.
 function calcFontSize(
   container: HTMLDivElement | null,
-  textProportion: number,
+  textProportion: number
 ) {
   const fallbackSize = "16px";
 
   if (container) {
     const containerWidth = container.offsetWidth;
-    const computedSize = `${containerWidth / textProportion}px`;
+    const computedSize = containerWidth / textProportion;
+    const finalFontSize = `${computedSize}px`;
 
-    return computedSize;
+    return computedSize >= 12 ? finalFontSize : "12px";
   } else {
     return fallbackSize;
   }
