2d3d144b175ad78e350f438903622f8dc50eee0a
Arturo López <jhonas29@gmail.com>
2025-04-22 19:21:36 +0200

    chore(map): Move all map TypeScript types to mapTypes.ts

---
+++ src/classes/map/mapTypes.ts
@@ -0,0 +1,39 @@
+export interface ReachableCell {
+  col: number;
+  row: number;
+}
+
+export type DoorsToShow = {
+  north: "none" | "open" | "closed";
+  east: "none" | "open" | "closed";
+  south: "none" | "open" | "closed";
+  west: "none" | "open" | "closed";
+};
+
+export type SomeDoorsToShow = {
+  north?: "none" | "open" | "closed";
+  east?: "none" | "open" | "closed";
+  south?: "none" | "open" | "closed";
+  west?: "none" | "open" | "closed";
+};
+
+export type NamePosition = "top" | "middle" | "bottom";
+
+export interface MapCellConfig {
+  id: number;
+  col: number;
+  row: number;
+  group?: string;
+  name?: string;
+  namePosition?: NamePosition;
+  visited?: boolean;
+  hidden?: boolean;
+  reachableCells?: ReachableCell[];
+  doorsToShow?: SomeDoorsToShow;
+  event?: string;
+  hasPuzzle?: boolean;
+  hasNpc?: boolean;
+  hasSpirit?: boolean;
+  imageSrc?: string;
+  items?: BaseItem[];
+}
