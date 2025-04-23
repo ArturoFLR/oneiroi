dfb34b1e43b827e2c498596f7b64d32adae2f145
Arturo López <jhonas29@gmail.com>
2025-04-23 10:18:46 +0200

    chore(playground): Add MapGenerator with demo data to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import SoundTesting from "../playground/sound/SoundTesting";
+import MapGenerator from "./components/map/MapGenerator";
+import nataliaHouseMapCells from "../playground/map/nataliaHouseMap";
 
 function App() {
   return (
     <>
       <SoundTesting />
+      <div className="mainContainer">
+        <h1>Mapa Casa de Natalia</h1>
+        <MapGenerator mapCells={nataliaHouseMapCells} />
+      </div>
     </>
   );
 }
