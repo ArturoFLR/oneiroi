366a5bf4595762e533f503fa2a89aaabdf652891
Arturo López <jhonas29@gmail.com>
2025-04-23 09:48:45 +0200

    styles(proyect): Add theme.ts for global fonts and colors

---
+++ src/theme.ts
@@ -0,0 +1,23 @@
+export const GLOBAL_FONTS = {
+  mapCellName: "Strait, sans-serif",
+};
+
+export const GLOBAL_COLORS = {
+  orange: {
+    glow: "#ffcc00",
+    highlightedText: "#fddd5c",
+    text: "#fddd5c",
+  },
+  map: {
+    backgrGradient1: "#02251a",
+    backgrGradient2: "#2e5968",
+    cellInnerBorder: "#61a4b4",
+    cellOuterBorder: "#000000",
+    cellBackground: "#45626cb7",
+  },
+  icons: {
+    bulbColor: "#fddd5c",
+    bulbGlow: "#ffcc00",
+    npcColor: "#9EB7E5",
+  },
+};
