5673f2a05d2338886d3804fd5b61f20fb2eaf613
Arturo López <jhonas29@gmail.com>
2025-04-23 09:01:45 +0200

    types(item): Add type definitions for Item class in itemTypes.ts

---
+++ src/classes/item/itemTypes.ts
@@ -0,0 +1,19 @@
+// Este interfaz define el objeto de configuración que debemos crear para instanciar un objeto BaseItem, donde sólo es necesario especificar las propiedades
+// que no vayan a tener el valor por defecto y los  métodos que vayan a hacer algo realmente útil. "name" y "description" sí son obligatorios, siempre cambian.
+export interface BaseItemConfig {
+  hidden?: boolean;
+  name: string;
+  description: string;
+  isLooked?: boolean;
+  isOpen?: boolean;
+  isPushed?: boolean;
+  isTaken?: boolean;
+  imageSrc?: string;
+
+  onLook?: () => string;
+  onTake?: () => string;
+  onOpen?: () => string;
+  onClose?: () => string;
+  onPush?: () => string;
+  useWith?: (item: BaseItem) => string;
+}
