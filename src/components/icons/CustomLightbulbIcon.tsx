624331ed2a494901676e15561031c8814af6587e
Arturo López <jhonas29@gmail.com>
2025-04-23 09:31:18 +0200

    feat(map): Add CustomLightbulbIcon component

---
+++ src/components/icons/CustomLightbulbIcon.tsx
@@ -0,0 +1,9 @@
+import { styled } from "@mui/system";
+import LightbulbIcon from "@mui/icons-material/Lightbulb";
+
+const CustomLightbulbIcon = styled(LightbulbIcon)({
+  color: "inherit",
+  fontSize: "inherit",
+});
+
+export default CustomLightbulbIcon;
