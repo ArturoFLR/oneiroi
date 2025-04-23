25867446e3ba0ffcb056f11622e7bf82d8c5f58d
Arturo López <jhonas29@gmail.com>
2025-04-23 09:39:42 +0200

    feat(map): Add CustomNpcIcon.tsx component

---
+++ src/components/icons/CustomNpcIcon.tsx
@@ -0,0 +1,9 @@
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import { styled } from "@mui/system";
+
+const CustomNpcIcon = styled(AccountCircleIcon)({
+  color: "inherit",
+  fontSize: "inherit",
+});
+
+export default CustomNpcIcon;
