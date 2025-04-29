b06abc6be1dcbd0aee083015e1176fd570f8b11e
Arturo López <jhonas29@gmail.com>
2025-04-29 12:41:01 +0200

    feat(cinematic): Add preload images feature to CinematicDirector

--- src/components/cinematics/CinematicDirector.tsx
+++ src/components/cinematics/CinematicDirector.tsx
@@ -1,7 +1,68 @@
-import MainFrame from "./styled/MainFrame";
+import { useCallback, useEffect, useRef, useState } from "react";
+import LoadingSpinner from "../common/LoadingSpinner";
+import ScreenDarkener from "../common/ScreenDarkener";
+import MainViewer from "./styled/MainViewer";
+import { CinematicSceneAuto } from "./cinematicTypes";
 
-function CinematicDirector() {
-  return <MainFrame></MainFrame>;
+interface CinematicDirectorProps {
+  cinematicData: CinematicSceneAuto;
+}
+
+function CinematicDirector({ cinematicData }: CinematicDirectorProps) {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [loadingProgress, setLoadingProgress] = useState<number>(0);
+  const [widePicture, setWidePicture] = useState<boolean>(true);
+  const [actualShotIndex, setActualShotIndex] = useState<number>(0);
+
+  const cinematicDataWithImgsRef = useRef<CinematicSceneAuto>(cinematicData);
+
+  const preloadImages = useCallback(() => {
+    let completionPercentage = 0;
+    const totalImages = cinematicDataWithImgsRef.current.length;
+
+    cinematicDataWithImgsRef.current.forEach((shot, index) => {
+      const advanceCompletion = () => {
+        completionPercentage += 100 / totalImages;
+        setLoadingProgress(completionPercentage);
+        if (completionPercentage >= 100) {
+          setIsLoading(false);
+        }
+      };
+
+      if (shot.mainImageUrl) {
+        const img = new Image();
+        img.src = shot.mainImageUrl; // Hace que la imagen se precargue en la caché del navegador, aunque no la guardemos en ningún sitio.
+        img.onload = () => {
+          cinematicDataWithImgsRef.current[index].mainImageElement = img;
+          advanceCompletion();
+          img.onerror = () => {
+            console.error(
+              `Cinematic Director: Error cargando la imagen: ${shot.mainImageUrl}`
+            );
+            advanceCompletion();
+          };
+        };
+      } else {
+        advanceCompletion();
+      }
+    });
+  }, []);
+
+  useEffect(() => {
+    if (isLoading) {
+      preloadImages();
+    }
+  }, [isLoading, preloadImages]);
+
+  return (
+    <ScreenDarkener color="dark">
+      {isLoading ? (
+        <LoadingSpinner progress={loadingProgress} />
+      ) : (
+        <MainViewer />
+      )}
+    </ScreenDarkener>
+  );
 }
 
 export default CinematicDirector;
