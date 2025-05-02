c8f5c2609883d95921fecfd5b7105de6edc17ddc
Arturo López <jhonas29@gmail.com>
2025-05-02 14:34:34 +0200

    test(cinematic): add CinematicTester.tsx to playground, to create audio context before testing a cinematic

---
+++ playground/cinematic/CinematicTester.tsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import { SoundDirectorAPI1 } from "../../src/classes/sound/singletons";
+import CinematicDirector from "../../src/components/cinematics/CinematicDirector";
+import styled from "styled-components";
+import { cinematicIntro } from "./cinematicDemo";
+
+const AudioContextButton = styled.button`
+  position: absolute;
+  top: 50%;
+  left: 40%;
+`;
+
+function CinematicTester() {
+  const [audioContextCreated, setAudioContextCreated] = useState(false);
+
+  function handleClick() {
+    SoundDirectorAPI1.initAudio();
+    setAudioContextCreated(true);
+  }
+
+  return (
+    <>
+      {audioContextCreated ? (
+        <CinematicDirector cinematicData={cinematicIntro} />
+      ) : (
+        <AudioContextButton onClick={handleClick}>
+          Crear Sound Context
+        </AudioContextButton>
+      )}
+    </>
+  );
+}
+
+export default CinematicTester;
