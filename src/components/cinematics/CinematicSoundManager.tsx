561a8d991f0fcfdd0b6a180ac118159f43b092a8
Arturo López <jhonas29@gmail.com>
2025-05-06 10:50:46 +0200

    feat(cinematics): add the ability to generate unique sounds and music for a shot to the CinematicSoundManager component
    - Several functions and an useEffect to manage them added to CinematicSoundManager

--- src/components/cinematics/CinematicSoundManager.tsx
+++ src/components/cinematics/CinematicSoundManager.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { CinematicSoundManagerData } from "./cinematicTypes";
+import { CinematicMusic, CinematicSoundManagerData } from "./cinematicTypes";
 import { useCallback, useEffect, useRef } from "react";
 import { SoundDirectorAPI1 } from "../../classes/sound/singletons";
 import { AudioEnvironment } from "../../classes/sound/soundTypes";
@@ -24,6 +24,7 @@ function CinematicSoundManager({
   lastShotSoundFadeDuration,
 }: CinematicSoundManagerProps) {
   const uniqueSoundsTimersRef = useRef<number[]>([]);
+  const musicTimersRef = useRef<number[]>([]);
 
   const generateUniqueSounds = useCallback(() => {
     if (!cinematicSoundData[actualShotIndex]?.uniqueSounds) return;
@@ -34,7 +35,7 @@ function CinematicSoundManager({
       if (sound.delay > 0) {
         const soundTimer = window.setTimeout(() => {
           SoundDirectorAPI1.playSound(
-            sound.env,
+            AudioEnvironment.Cinematic,
             sound.category,
             sound.soundName,
             sound.soundSrc,
@@ -45,7 +46,7 @@ function CinematicSoundManager({
         uniqueSoundsTimersRef.current.push(soundTimer);
       } else {
         SoundDirectorAPI1.playSound(
-          sound.env,
+          AudioEnvironment.Cinematic,
           sound.category,
           sound.soundName,
           sound.soundSrc,
@@ -56,10 +57,102 @@ function CinematicSoundManager({
     });
   }, [cinematicSoundData, actualShotIndex]);
 
+  const stopPreviousMusic = useCallback((fadeDuration: number) => {
+    if (fadeDuration > 0) {
+      SoundDirectorAPI1.fadeCategory(AudioEnvironment.Cinematic, "music", {
+        final: 0,
+        milliseconds: fadeDuration,
+      });
+    } else {
+      SoundDirectorAPI1.stopCategorySounds(AudioEnvironment.Cinematic, "music");
+    }
+  }, []);
+
+  const generateNewMusic = useCallback((musicData: CinematicMusic) => {
+    const defaultOldMusicFadeDuration = 1000;
+    const defaultNewMusicFadeOutDuration = 3500;
+    const newMusicFadeOutDuration = musicData?.fadeOutDuration
+      ? musicData.fadeOutDuration
+      : defaultNewMusicFadeOutDuration;
+    const newMusicDelay = musicData.delay;
+
+    // Hacemos fade out de la música de planos anteriores, si la hay.
+    SoundDirectorAPI1.fadeCategory(AudioEnvironment.Cinematic, "music", {
+      final: 0,
+      milliseconds: defaultOldMusicFadeDuration,
+    });
+
+    //Reproducimos la nueva música, con el delay indicado por el usuario.
+    const musicStartTimer = window.setTimeout(() => {
+      //Aplicamos loop si se especifica
+      if (musicData.loop) {
+        SoundDirectorAPI1.createLoopWhithFade(
+          AudioEnvironment.Cinematic,
+          "music",
+          musicData.soundName,
+          musicData.soundSrc,
+          2500,
+          1000,
+          musicData.config,
+          musicData.stereo
+        );
+      } else {
+        SoundDirectorAPI1.playSound(
+          AudioEnvironment.Cinematic,
+          "music",
+          musicData.soundName,
+          musicData.soundSrc,
+          musicData.config,
+          musicData.stereo
+        );
+      }
+    }, newMusicDelay);
+
+    //Si se ha especificado un tiempo concreto en el que la música debe parar, se aplica:
+    if (musicData.endTime) {
+      const musicEndTimer = window.setTimeout(() => {
+        SoundDirectorAPI1.fadeCategory(AudioEnvironment.Cinematic, "music", {
+          final: 0,
+          milliseconds: newMusicFadeOutDuration,
+        });
+      }, musicData.endTime);
+
+      musicTimersRef.current.push(musicEndTimer);
+    }
+
+    musicTimersRef.current.push(musicStartTimer);
+  }, []);
+
+  // Genera sonidos únicos y música
   useEffect(() => {
     if (isLoading) return;
+
+    //Sonidos únicos
     generateUniqueSounds();
-  }, [isLoading, generateUniqueSounds]);
+
+    //Música
+    const actualShotSoundData = cinematicSoundData[actualShotIndex];
+
+    if (!actualShotSoundData?.music) return;
+
+    if (typeof actualShotSoundData.music === "number") {
+      stopPreviousMusic(actualShotSoundData.music);
+    }
+
+    if (
+      actualShotSoundData.music &&
+      typeof actualShotSoundData.music !== "number"
+    ) {
+      generateNewMusic(actualShotSoundData.music);
+    }
+  }, [
+    isLoading,
+    generateUniqueSounds,
+    cinematicSoundData,
+    actualShotIndex,
+    stopPreviousMusic,
+    generateNewMusic,
+  ]);
 
   //Si este es el último plano de la cinemática, aplicamos un fade-out a sonidos y música antes de que termine el plano => evitamos que la cinemática termine y sigan sonando.
   useEffect(() => {
@@ -87,9 +180,14 @@ function CinematicSoundManager({
   // Elimina todos los timers pendientes al desmontar el componente.
   useEffect(() => {
     const uniqueSoundsTimersToClear = uniqueSoundsTimersRef.current;
+    const musicTimersToClear = musicTimersRef.current;
 
     return () => {
       uniqueSoundsTimersToClear.forEach((timer) => {
+        window.clearTimeout(timer);
+      });
+
+      musicTimersToClear.forEach((timer) => {
         window.clearTimeout(timer);
       });
     };
