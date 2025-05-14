35922ea9849712390eea3b70fc1ec4be8be3657d
Arturo López <jhonas29@gmail.com>
2025-05-14 14:43:57 +0200

    feat(cinematics): add logic to CinematicFxFrame component so it can use the RainFx component.

--- src/components/cinematics/styled/CinematicFxFrame.tsx
+++ src/components/cinematics/styled/CinematicFxFrame.tsx
@@ -1,7 +1,14 @@
 import LightningFX from "../../common/fxAndFilters/LightningFX";
 import styled from "styled-components";
 import { CinematicFXData } from "../cinematicTypes";
-import { useCallback, useEffect, useRef } from "react";
+import {
+  useCallback,
+  useEffect,
+  useLayoutEffect,
+  useRef,
+  useState,
+} from "react";
+import RainFx from "../../common/fxAndFilters/RainFx";
 
 const MainContainer = styled.div`
   position: absolute;
@@ -28,7 +35,8 @@ function CinematicFxFrame({
   currentShotFx,
   nextShotFx,
 }: CinematicFxFrameProps) {
-  const lightningTimeoutsRef = useRef<number[]>([]);
+  const [showRain, setShowRain] = useState<boolean>(false);
+  const rainfallTimeoutsRef = useRef<number[]>([]);
 
   ////////////////////////////////////////////    LIGHTNING FX   ////////////////////////////////////////////////////
   //Este fx sólo se va a generar en el current shot, nunca en el next shot (no funciona bien el efecto durante los fades)
@@ -37,9 +45,13 @@ function CinematicFxFrame({
     if (currentShotFx?.lightning && isForCurrentShot) {
       const lightningComponentsToShow: React.ReactNode[] = [];
 
-      currentShotFx.lightning.map((lightningData) => {
+      currentShotFx.lightning.map((lightningData, index) => {
         lightningComponentsToShow.push(
-          <LightningFX size={lightningData.size} delay={lightningData.delay} />
+          <LightningFX
+            size={lightningData.size}
+            delay={lightningData.delay}
+            id={`fxLightning${index}`}
+          />
         );
       });
       return lightningComponentsToShow;
@@ -48,16 +60,81 @@ function CinematicFxFrame({
     }
   }, [currentShotFx, isForCurrentShot]);
 
+  ////////////////////////////////////////////    RAINFALL FX   ////////////////////////////////////////////////////
+
+  const generateRainfallFx = useCallback(() => {
+    if (isForCurrentShot && currentShotFx?.rain) {
+      const rainData = currentShotFx.rain;
+      return (
+        <RainFx
+          intensity={rainData.intensity}
+          size={rainData.size}
+          isStarting={rainData.isStarting}
+        />
+      );
+    } else if (
+      !isForCurrentShot &&
+      nextShotFx?.rain &&
+      nextShotFx.rain.delay === 0
+    ) {
+      const rainData = nextShotFx.rain;
+
+      return (
+        <RainFx
+          intensity={rainData.intensity}
+          size={rainData.size}
+          isStarting={rainData.isStarting}
+        />
+      );
+    } else {
+      return null;
+    }
+  }, [isForCurrentShot, currentShotFx?.rain, nextShotFx?.rain]);
+
+  //Activa o desactiva la lluvia usando "showRain"
+  useLayoutEffect(() => {
+    //Si no hay datos para lluvia, la desactivamos
+    if (
+      (isForCurrentShot && !currentShotFx?.rain) ||
+      (!isForCurrentShot && (!nextShotFx?.rain || nextShotFx.rain.delay > 0))
+    ) {
+      setShowRain(false);
+    }
+
+    //Si existen datos, dependiendo del delay, ejecutamos o programamos un timeout
+    if (isForCurrentShot && currentShotFx?.rain) {
+      if (currentShotFx.rain.delay > 0) {
+        const rainTimeout = window.setTimeout(() => {
+          setShowRain(true);
+        }, currentShotFx?.rain?.delay);
+
+        rainfallTimeoutsRef.current.push(rainTimeout);
+      } else {
+        setShowRain(true);
+      }
+    }
+
+    if (!isForCurrentShot && nextShotFx?.rain && nextShotFx.rain.delay === 0) {
+      setShowRain(true);
+    }
+  }, [isForCurrentShot, currentShotFx?.rain, nextShotFx?.rain]);
+
+  // Limpieza de timeouts
   useEffect(() => {
-    const timersToClear = lightningTimeoutsRef.current;
+    const timersToClear = rainfallTimeoutsRef.current;
     return () => {
       timersToClear.forEach((timer) => {
         window.clearTimeout(timer);
       });
     };
-  }, [currentShotFx]);
+  }, []);
 
-  return <MainContainer>{generateLightningFx()}</MainContainer>;
+  return (
+    <MainContainer>
+      {generateLightningFx()}
+      {showRain ? generateRainfallFx() : null}
+    </MainContainer>
+  );
 }
 
 export default CinematicFxFrame;
