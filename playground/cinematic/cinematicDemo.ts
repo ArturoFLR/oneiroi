3234e5da62cce61c196ff7465a077e0e164e6ac7
Arturo López <jhonas29@gmail.com>
2025-05-19 11:10:06 +0200

    test(cinematics): add new VideoFx and TextCaption effects to the shots in cinematicDemo.ts

--- playground/cinematic/cinematicDemo.ts
+++ playground/cinematic/cinematicDemo.ts
@@ -16,6 +16,12 @@ import {
 import alienSunset1 from "@assets/graphics/cinematics/intro/alien-landscape_01.webp";
 import alienRuins1 from "@assets/graphics/cinematics/intro/alien-landscape_05.webp";
 import stormyClouds1 from "@assets/graphics/cinematics/intro/stormy-clouds_03.webp";
+import stormyClouds2 from "@assets/graphics/cinematics/intro/stormy-clouds_02.webp";
+import ruins1 from "@assets/graphics/cinematics/intro/alien-landscape_06.webp";
+import ambientSmoke from "@assets/graphics/videofx/atmosphere/Atmosphere_06.webm";
+import fume1 from "@assets/graphics/videofx/effects/Particle_Stream_01.webm";
+import fume2 from "@assets/graphics/videofx/effects/Particle_Stream_02.webm";
+import { GLOBAL_FONTS } from "../../src/theme";
 
 export const cinematicIntro: CinematicSceneAuto = [
   {
@@ -23,10 +29,23 @@ export const cinematicIntro: CinematicSceneAuto = [
     backgroundColor: "#000000",
     widePicture: true,
     shotTransition: "fade",
-    shotDuration: 9000,
+    shotDuration: 10000,
     fadeDuration: 5500,
     ambientSound: shot1AmbientSound,
     music: shot1Music,
+    specialFX: {
+      textCaption: [
+        {
+          isZoomable: false,
+          text: "No quiero estar aquí.",
+          fontSize: 2.5,
+          fontFamily: GLOBAL_FONTS.textCinematics.narratorEmotional,
+          duration: 4500,
+          fadeInDuration: 2000,
+          fadeOutDuration: 300,
+        },
+      ],
+    },
   },
   {
     id: 2,
@@ -50,6 +69,29 @@ export const cinematicIntro: CinematicSceneAuto = [
       },
       animType: "ease-in",
     },
+    specialFX: {
+      textCaption: [
+        {
+          isZoomable: false,
+          text: "El sol se está poniendo...",
+          fontSize: "medium",
+          fontFamily: GLOBAL_FONTS.textCinematics.narratorEmotional,
+          fadeInDuration: 2000,
+          delay: 1,
+        },
+        {
+          isZoomable: false,
+          text: "y no quiero estar aquí.",
+          fontFamily: GLOBAL_FONTS.textCinematics.narratorEmotional,
+          delay: 2000,
+          position: "center",
+          positionTop: 11,
+          positionLeft: 10,
+          fontSize: "medium",
+          fadeInDuration: 1000,
+        },
+      ],
+    },
     // specialActions: function () {
     //   const timer1 = window.setTimeout(() => {
     //     SoundDirectorAPI1.setSoundRate(
@@ -64,12 +106,6 @@ export const cinematicIntro: CinematicSceneAuto = [
     //   }, 3500);
     // },
     specialActionsTimeouts: [],
-    specialFX: {
-      tremor: {
-        delay: 2000,
-        intensity: "medium",
-      },
-    },
   },
   {
     id: 3,
@@ -77,7 +113,7 @@ export const cinematicIntro: CinematicSceneAuto = [
     mainImageAlt: "Ruinas en un paisaje alienígena",
     widePicture: true,
     shotTransition: "cut",
-    shotDuration: 6000,
+    shotDuration: 8000,
     // music: shot3Music,
     zoom: {
       zoomStartSize: 1.4,
@@ -88,34 +124,113 @@ export const cinematicIntro: CinematicSceneAuto = [
       zoomEndSize: 1.4,
       zoomEndPosition: {
         top: 0,
-        left: -10,
+        left: -15,
       },
       animType: "linear",
     },
     specialFX: {
-      tremor: {
-        delay: 0,
-        intensity: "low",
-      },
+      textCaption: [
+        {
+          isZoomable: true,
+          text: "Entre sus ruinas",
+          delay: 1500,
+          duration: 4500,
+          fadeInDuration: 1200,
+          fadeOutDuration: 2000,
+          position: "center",
+          positionLeft: 10,
+          positionTop: 5,
+          fontSize: "small",
+          fontFamily: GLOBAL_FONTS.textCinematics.narratorEmotional,
+          zIndex: 0,
+        },
+      ],
+      lightning: [{ isZoomable: true, delay: 0, size: "medium" }],
+      videoFx: [
+        {
+          isZoomable: true,
+          src: fume2,
+          size: "70%",
+          positionTop: "15%",
+          positionLeft: "24%",
+          initialFadeDuration: 0,
+          finalFadeDuration: 0,
+          delay: 500,
+          loop: true,
+          speed: 0.8,
+          extraCss: "mix-blend-mode: exclusion;",
+        },
+        {
+          isZoomable: true,
+          src: fume2,
+          size: "70%",
+          positionTop: "15%",
+          positionLeft: "24%",
+          initialFadeDuration: 0,
+          finalFadeDuration: 0,
+          delay: 500,
+          loop: true,
+          speed: 0.8,
+          extraCss: "mix-blend-mode: exclusion;",
+        },
+        {
+          isZoomable: true,
+          src: fume2,
+          size: "75%",
+          positionTop: "15%",
+          positionLeft: "30%",
+          initialFadeDuration: 0,
+          finalFadeDuration: 0,
+          delay: 2400,
+          loop: true,
+          speed: 0.75,
+          extraCss: "mix-blend-mode: exclusion;",
+        },
+        {
+          isZoomable: true,
+          src: fume2,
+          size: "75%",
+          positionTop: "15%",
+          positionLeft: "30%",
+          initialFadeDuration: 0,
+          finalFadeDuration: 0,
+          delay: 2400,
+          loop: true,
+          speed: 0.75,
+          extraCss: "mix-blend-mode: exclusion;",
+        },
+        {
+          isZoomable: true,
+          src: fume2,
+          size: "75%",
+          positionTop: "15%",
+          positionLeft: "36%",
+          initialFadeDuration: 0,
+          finalFadeDuration: 0,
+          delay: 2800,
+          loop: true,
+          speed: 0.65,
+          extraCss: "mix-blend-mode: exclusion;",
+        },
+      ],
     },
   },
   {
     id: 4,
-    mainImageUrl: stormyClouds1,
+    mainImageUrl: stormyClouds2,
     mainImageAlt: "Un relámpago ilumina un cúmulo de nubes",
-    widePicture: false,
-    shotTransition: "fade",
-    shotDuration: 10000,
-    fadeDuration: 4000,
+    widePicture: true,
+    shotTransition: "cut",
+    shotDuration: 8000,
     uniqueSounds: shot3UniqueSounds,
     // music: shot3Music,
     zoom: {
-      zoomStartSize: 1.3,
+      zoomStartSize: 1,
       zoomStartPosition: {
         top: 0,
         left: 0,
       },
-      zoomEndSize: 1.7,
+      zoomEndSize: 1.35,
       zoomEndPosition: {
         top: 15,
         left: 0,
@@ -123,16 +238,103 @@ export const cinematicIntro: CinematicSceneAuto = [
       animType: "linear",
     },
     specialFX: {
-      tremor: {
-        delay: 2000,
+      // tremor: {
+      //   delay: 0,
+      //   intensity: "medium",
+      // },
+      lightning: [
+        { isZoomable: true, delay: 0, size: "big" },
+        { isZoomable: true, delay: 4000, size: "small" },
+      ],
+      rain: {
+        isZoomable: false,
+        delay: 0,
         intensity: "medium",
+        isStarting: false,
+        size: 0.8,
       },
     },
   },
   {
     id: 5,
+    mainImageUrl: ruins1,
+    mainImageAlt: "Unas ruinas en ambiente nocturno",
+    widePicture: true,
+    shotTransition: "fade",
+    shotDuration: 12000,
+    fadeDuration: 4000,
+    // uniqueSounds: shot3UniqueSounds,
+    // music: shot3Music,
+    zoom: {
+      animType: "ease-in",
+      zoomStartSize: 1,
+      zoomStartPosition: {
+        top: 0,
+        left: 0,
+      },
+      zoomEndSize: 1.7,
+      zoomEndPosition: {
+        top: -10,
+        left: -20,
+      },
+    },
+    specialFX: {
+      // tremor: {
+      //   delay: 0,
+      //   intensity: "medium",
+      // },
+      textCaption: [
+        {
+          isZoomable: false,
+          text: "El Barraco, 1936",
+          position: "bottom",
+          positionLeft: -30,
+          fontSize: 3,
+          duration: 7000,
+          fadeInDuration: 0,
+          fadeOutDuration: 800,
+        },
+      ],
+      lightning: [{ isZoomable: true, delay: 2000, size: "medium" }],
+      rain: {
+        isZoomable: false,
+        delay: 0,
+        intensity: "high",
+        isStarting: false,
+        size: 0.8,
+      },
+      videoFx: [
+        {
+          isZoomable: true,
+          src: ambientSmoke,
+          size: "100%",
+          positionTop: "0%",
+          positionLeft: "0%",
+          initialFadeDuration: 0,
+          finalFadeDuration: 0,
+          delay: 0,
+          loop: false,
+          speed: 0.7,
+        },
+        {
+          isZoomable: true,
+          src: ambientSmoke,
+          size: "100%",
+          positionTop: "0%",
+          positionLeft: "0%",
+          initialFadeDuration: 0,
+          finalFadeDuration: 0,
+          delay: 0,
+          loop: false,
+          speed: 0.7,
+        },
+      ],
+    },
+  },
+  {
+    id: 6,
     backgroundColor: "#000000",
-    widePicture: false,
+    widePicture: true,
     onEndAudioFadeDuration: 2000,
     onEnd: () => {
       const soundStoreData = SoundStore1.audioStore.cinematic;
