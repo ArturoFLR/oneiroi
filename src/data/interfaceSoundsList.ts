af75800e0028af05610c36b6561e84528cad8b9e
Arturo López <jhonas29@gmail.com>
2025-05-22 09:12:39 +0200

    chore(sound): add interfaceSoundsList.ts, a list of interface sounds that will be used to preload them

---
+++ src/data/interfaceSoundsList.ts
@@ -0,0 +1,8 @@
+import { InterfaceSoundData } from "src/components/mainMenu/mainMenuTypes";
+
+export const interfaceSoundsList: InterfaceSoundData[] = [
+  {
+    soundName: "click1",
+    soundSrc: "../assets/audio/sounds/interface/interface-click.mp3",
+  },
+];
