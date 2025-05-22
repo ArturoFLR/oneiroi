02a3400c75056cbd68232fb1babb878824c8c484
Arturo López <jhonas29@gmail.com>
2025-05-22 19:08:27 +0200

    feat(global): add GameDirector component

---
+++ src/components/GameDirector.tsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+import ModalOneButton from "./common/modals/ModalOneButton";
+import { SoundDirectorAPI1 } from "../classes/sound/singletons";
+import MainMenu from "./mainMenu/MainMenu";
+
+export type GameMainState = "init" | "mainMenu"; //Añadir según sea necesario
+
+function GameDirector() {
+  const [gameMainState, setGameMainState] = useState<GameMainState>("init");
+
+  function handleAudioInitClick() {
+    SoundDirectorAPI1.initAudio();
+    setGameMainState("mainMenu");
+  }
+
+  useEffect(() => {
+    const handleBeforeUnload = () => {
+      SoundDirectorAPI1.stopAllSounds();
+    };
+    window.addEventListener("beforeunload", handleBeforeUnload);
+    return () => {
+      window.removeEventListener("beforeunload", handleBeforeUnload);
+    };
+  }, []);
+
+  return (
+    <>
+      {gameMainState === "init" && (
+        <>
+          <ModalOneButton
+            mainText="No borres la caché del navegador, o perderás tus partidas guardadas"
+            secondaryText="Si juegas desde un móvil, te recomendamos usar auriculares"
+            buttonText="Continuar"
+            onClick={handleAudioInitClick}
+            screenDarkenerColor="black"
+          />
+        </>
+      )}
+
+      {gameMainState === "mainMenu" && <MainMenu />}
+    </>
+  );
+}
+
+export default GameDirector;
