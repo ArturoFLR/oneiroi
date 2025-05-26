ff4b329a9f68179cafdc3ed0463e50de8943a569
Arturo López <jhonas29@gmail.com>
2025-05-26 12:36:42 +0200

    feat(main-menu): change the main state to "cinematic" if the user haven´t seen the intro, from MainMenu. Use local storage to track this.
    - Now the MainMenu component uses the ModalOneButton component to show a warning message if the local storage isn´t available.

--- src/components/mainMenu/MainMenu.tsx
+++ src/components/mainMenu/MainMenu.tsx
@@ -3,14 +3,48 @@ import MainMenuPreloader from "./MainMenuPreloader";
 import MainMenuBg from "./MainMenuBg";
 import { SoundDirectorAPI1 } from "../../classes/sound/singletons";
 import { AudioEnvironment } from "../../classes/sound/soundTypes";
+import { useAppDispatch } from "../../store/hooks/reduxHooks";
+import { setMainState } from "../../store/slices/mainStateSlice";
+import {
+  isLocalStorageAvailable,
+  loadFromLocalStorage,
+  saveToLocalStorage,
+} from "../../utils/localStorageManipulation";
 
 import mainMenuMusic from "@assets/audio/music/crystal-oasis.mp3";
 import gongSound from "@assets/audio/sounds/effects/gong/gong.mp3";
+import ModalOneButton from "../common/modals/ModalOneButton";
 
 function MainMenu() {
   const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [isStorageModalShown, setIsStorageModalShown] =
+    useState<boolean>(false);
+
   const isMusicPlaying = useRef<boolean>(false);
 
+  //Redux
+  const dispatch = useAppDispatch();
+  //Redux Fin
+
+  function handleStorageModalClick() {
+    setIsStorageModalShown(false);
+  }
+
+  //Si el usuario no ha visto la cinemática de introducción, la reproduce.
+  useLayoutEffect(() => {
+    if (isLocalStorageAvailable()) {
+      const hasUserSeenIntro = loadFromLocalStorage("hasSeenIntro");
+
+      if (!hasUserSeenIntro) {
+        saveToLocalStorage("hasSeenIntro", "true");
+        dispatch(setMainState("cinematic"));
+      }
+    } else {
+      setIsStorageModalShown(true);
+    }
+  }, [dispatch]);
+
+  //Se encarga de gestionar música y sonido en el main menu
   useLayoutEffect(() => {
     if (isLoading) {
       SoundDirectorAPI1.stopAllSounds();
@@ -55,10 +89,22 @@ function MainMenu() {
 
   return (
     <>
-      {isLoading ? (
-        <MainMenuPreloader setIsLoading={setIsLoading} />
-      ) : (
-        <MainMenuBg />
+      {isLoading && <MainMenuPreloader setIsLoading={setIsLoading} />}
+
+      {!isLoading && (
+        <>
+          <MainMenuBg />
+
+          {isStorageModalShown && (
+            <ModalOneButton
+              screenDarkenerColor="dark"
+              mainText="No ha sido posible usar el almacenamiento de tu navegador, por lo que algunas funciones, como cargar / salvar partida, no estarán disponibles."
+              secondaryText="Asegúrate de no estar navegando en modo incógnito y de tener el 'local storage' habilitado en tu navegador."
+              buttonText="Entendido"
+              onClick={handleStorageModalClick}
+            />
+          )}
+        </>
       )}
     </>
   );
