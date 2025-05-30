2b5d6cf78c4159f1ba831422c25ff862432ef7ff
Arturo López <jhonas29@gmail.com>
2025-05-30 14:04:45 +0200

    feat(store): add cinematicDataReducer to the store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import mainStateReducer from "./slices/mainStateSlice";
+import cinematicDataReducer from "./slices/cinematicSlice";
 
 export const store = configureStore({
   reducer: {
     mainState: mainStateReducer,
+    cinematicData: cinematicDataReducer,
   },
 });
 
