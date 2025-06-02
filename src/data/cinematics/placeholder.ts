7ef1795f1b68f367a31265d9e92ed3fa1ca0d88a
Arturo López <jhonas29@gmail.com>
2025-06-02 18:00:29 +0200

    feat(cinematics): add placeholder cinematic data in placeholder.ts
    - Used as the default cinematic data.

---
+++ src/data/cinematics/placeholder.ts
@@ -0,0 +1,14 @@
+import { GLOBAL_COLORS } from "../../theme";
+import { CinematicSceneAuto } from "../../components/cinematics/cinematicTypes";
+
+// Esta cinemática no es más que un plano en negro. Se usa como valor inicial de cinematicData en cinematicSlice.ts, y se usa para
+// evitar un error en la store, que se producía si se usaba como valor inicial una cinemática que usara la store en el código de la
+// propiedad "onEnd()" en su último plano. Esto producía una importación circular.
+
+export const placeholderCinematic: CinematicSceneAuto = [
+  {
+    id: 1,
+    backgroundColor: GLOBAL_COLORS.black,
+    widePicture: true,
+  },
+];
