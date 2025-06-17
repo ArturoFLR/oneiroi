7b63d86b8d55652ea6d9f9bb55e74ad38890a314
Arturo López <jhonas29@gmail.com>
2025-06-17 11:45:25 +0200

    feat(cinematics): Add the necessary logic to NPCPortrait component to be controlled by ManualTextAndNPC component.
    - Window resize logic deleted (now is passed as props)
    - Fade animations and the logic to control them added

--- src/components/common/npcs/NPCPortrait.tsx
+++ src/components/common/npcs/NPCPortrait.tsx
@@ -1,9 +1,32 @@
-import { useLayoutEffect, useState } from "react";
-import styled, { css } from "styled-components";
+import styled, { css, keyframes } from "styled-components";
 import DistortionWrapper from "../fxAndFilters/DistortionWrapper";
 import { GLOBAL_COLORS } from "../../../theme";
+import { DistortionValues } from "../../cinematics/cinematicTypes";
+
+const fadeInAnim = keyframes`
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+`;
+
+const fadeOutAnim = keyframes`
+  0% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+  }
+`;
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 interface MainContainerProps {
+  $fadeInAnim: boolean;
+  $fadeOutAnim: boolean;
+  $animationDuration: number;
   $centered: boolean;
 }
 
@@ -23,7 +46,19 @@ const MainContainer = styled.div<MainContainerProps>`
       `;
   }}
 
-  z-index: 6;
+  ${({ $fadeOutAnim, $fadeInAnim, $animationDuration }) => {
+    if ($fadeOutAnim) {
+      return css`
+        animation: ${fadeOutAnim} ${$animationDuration}ms ease-in forwards;
+      `;
+    } else if ($fadeInAnim) {
+      return css`
+        animation: ${fadeInAnim} ${$animationDuration}ms ease-out forwards;
+      `;
+    } else return null;
+  }}
+
+  z-index: 1;
 `;
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -36,45 +71,45 @@ interface ImgContainerProps {
   $positionBottom: number | undefined;
   $positionLeft: number | undefined;
   $positionRight: number | undefined;
-  $landscapeWidth: number;
-  $portraitWidth: number;
+  $landscapeWidth: string;
+  $portraitWidth: string;
 }
 
-const ImgContainer = styled.img<ImgContainerProps>`
+const ImgContainer = styled.div<ImgContainerProps>`
   position: ${({ $centered }) => ($centered ? "relative" : "absolute")};
 
   ${({ $positionTop }) =>
     typeof $positionTop === "number"
       ? css`
-          ${$positionTop}%
+          top: ${$positionTop}%;
         `
       : null}
 
   ${({ $positionBottom }) =>
     typeof $positionBottom === "number"
       ? css`
-          ${$positionBottom}%
+          bottom: ${$positionBottom}%;
         `
       : null}
 
 	${({ $positionLeft }) =>
     typeof $positionLeft === "number"
       ? css`
-          ${$positionLeft}%
+          left: ${$positionLeft}%;
         `
       : null}
 
 	${({ $positionRight }) =>
     typeof $positionRight === "number"
       ? css`
-          ${$positionRight}%
+          right: ${$positionRight}%;
         `
       : null}
 
 	width: ${({ $windowWidth, $windowHeight, $landscapeWidth, $portraitWidth }) =>
     $windowWidth >= $windowHeight ? $landscapeWidth : $portraitWidth};
 
-  border: 2px solid ${GLOBAL_COLORS.white};
+  border: 3px solid ${GLOBAL_COLORS.white};
 `;
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -90,15 +125,12 @@ const ImgStyled = styled.img`
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-interface DistortionValues {
-  initialValues?: number[];
-  maxValues?: number[];
-  increment?: number;
-  speed?: number;
-  intensity?: number;
-}
-
 interface NPCPortraitProps {
+  fadeInAnim: boolean;
+  fadeOutAnim: boolean;
+  animationsDuration: number;
+  windowWidth: number;
+  windowHeight: number;
   imageSrc: string;
   landscapeWidth?: number;
   portraitWidth?: number;
@@ -112,6 +144,11 @@ interface NPCPortraitProps {
 }
 
 function NPCPortrait({
+  fadeInAnim,
+  fadeOutAnim,
+  animationsDuration,
+  windowWidth,
+  windowHeight,
   imageSrc,
   landscapeWidth,
   portraitWidth,
@@ -123,37 +160,23 @@ function NPCPortrait({
   distortionAnim,
   distortionValues,
 }: NPCPortraitProps) {
-  const [windowSize, setWindowSize] = useState<number[]>([0, 0]);
-
-  // Calcula la proporción de la pantalla, y establece un listener
-  // para que se recalcule si hay un "resize" de la pantalla.
-  useLayoutEffect(() => {
-    function setNewWindowSize() {
-      setWindowSize([window.innerWidth, window.innerHeight]);
-    }
-
-    // Hace el cálculo inicial
-    setNewWindowSize();
-
-    window.addEventListener("resize", setNewWindowSize);
-
-    return () => {
-      window.removeEventListener("resize", setNewWindowSize);
-    };
-  }, []);
-
   //Valores por defecto:
   const finalLandscapeWidth =
-    typeof landscapeWidth === "number" ? landscapeWidth : 25;
+    typeof landscapeWidth === "number" ? `${landscapeWidth}%` : "25%";
   const finalPortraitWidth =
-    typeof portraitWidth === "number" ? portraitWidth : 35;
+    typeof portraitWidth === "number" ? `${portraitWidth}%` : "35%";
   const finalCentered = centered === true ? centered : false;
 
   return (
-    <MainContainer $centered={finalCentered}>
+    <MainContainer
+      $centered={finalCentered}
+      $fadeInAnim={fadeInAnim}
+      $fadeOutAnim={fadeOutAnim}
+      $animationDuration={animationsDuration}
+    >
       <ImgContainer
-        $windowWidth={windowSize[0]}
-        $windowHeight={windowSize[1]}
+        $windowWidth={windowWidth}
+        $windowHeight={windowHeight}
         $centered={finalCentered}
         $landscapeWidth={finalLandscapeWidth}
         $portraitWidth={finalPortraitWidth}
