929c5dd66eedbc6c28526b67d40437801f67ab1c
Arturo López <jhonas29@gmail.com>
2025-08-24 11:42:16 +0200

    feat(scenarios): add stage, visited, and solved properties to Scenario class
    - name property type changed to ScenarioNameType

--- src/classes/scenarios/Scenario.ts
+++ src/classes/scenarios/Scenario.ts
@@ -1,10 +1,13 @@
 import { Map } from "../map/mapTypes";
-import { ScenarioConfig } from "./scenariosTypes";
+import { ScenarioConfig, ScenarioNameType } from "./scenariosTypes";
 
 export default class Scenario {
-  name: string;
+  name: ScenarioNameType;
   map: Map;
+  stage: number; // Etapa del escenario, para controlar el progreso realizado en él
   startingCellId: number; // ID de la celda donde comienza el escenario
+  visited: boolean;
+  solved: boolean;
 
   onArrive: () => void; // Función que se ejecuta al llegar al escenario
   onLeave: () => void; // Función que se ejecuta al salir del escenario
@@ -12,7 +15,11 @@ export default class Scenario {
   constructor(config: ScenarioConfig) {
     this.name = config.name;
     this.map = config.map;
+    this.stage = config.stage;
     this.startingCellId = config.startingCellId;
+    this.visited = config.visited;
+    this.solved = config.solved;
+
     this.onArrive =
       config.onArrive ?? (() => console.log(`Llegaste a ${this.name}`));
     this.onLeave =
--- src/classes/scenarios/scenariosTypes.ts
+++ src/classes/scenarios/scenariosTypes.ts
@@ -1,9 +1,18 @@
 import { Map } from "../map/mapTypes";
 
+export type ScenarioNameType =
+  | "Parque Aliseda"
+  | "Casa de Natalia"
+  | "Casa de Julián";
+
 export interface ScenarioConfig {
-  name: string;
+  name: ScenarioNameType;
   map: Map;
+  stage: number; // Etapa del escenario, para controlar el progreso realizado en él
   startingCellId: number; // ID de la celda donde comienza el escenario
+  visited: boolean;
+  solved: boolean;
+
   onArrive: () => void; // Función que se ejecuta al llegar al escenario
   onLeave: () => void; // Función que se ejecuta al salir del escenario
 }
