c3577a95afd9877afd3497b32f1ceeff28f47200
Arturo López <jhonas29@gmail.com>
2025-11-11 18:59:52 +0100

    feat(cinematics): add alisedaParkFail01 and nataliaHouseIntro01 cinematics to cinematicsMap.ts

--- src/data/cinematics/cinematicsMap.ts
+++ src/data/cinematics/cinematicsMap.ts
@@ -2,9 +2,13 @@ import { CinematicScene } from "../../components/cinematics/cinematicTypes";
 import { introCinematic } from "./intro/intro";
 import { CinematicName } from "../../store/slices/cinematicSlice";
 import { alisedaParkCinematic } from "./01_aliseda_park/alisedaPark";
+import { alisedaParkFail01Cinematic } from "./02_aliseda_park_fail_01/alisedaParkFail01";
+import { nataliaHouseIntro01Cinematic } from "./03_natalia_house_intro/nataliaHouseIntro01";
 
 // Añadir aquí la correlación entre nombre único de cada cinemática y la variable que la contiene.
 export const cinematicsMap: Record<CinematicName, CinematicScene> = {
   intro: introCinematic,
   alisedaPark01: alisedaParkCinematic,
+  alisedaParkFail01: alisedaParkFail01Cinematic,
+  nataliaHouseIntro01: nataliaHouseIntro01Cinematic,
 };
