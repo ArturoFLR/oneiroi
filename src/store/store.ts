74e0e03087810de377fb4d20de29893fb5dde5e4
Arturo López <jhonas29@gmail.com>
2025-11-11 19:26:14 +0100

    feat(store): add "scenario" and "sound" slices to the Redux store

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,12 +2,16 @@ import { configureStore } from "@reduxjs/toolkit";
 import mainStateReducer from "./slices/mainStateSlice";
 import cinematicDataReducer from "./slices/cinematicSlice";
 import aiChatDataReducer from "./slices/aiChatSlice";
+import scenarioDataReducer from "./slices/scenarioSlice";
+import soundDataReducer from "./slices/soundSlice";
 
 export const store = configureStore({
   reducer: {
     mainState: mainStateReducer,
     cinematicData: cinematicDataReducer,
     aiChatData: aiChatDataReducer,
+    scenarioData: scenarioDataReducer,
+    soundData: soundDataReducer,
   },
 });
 
