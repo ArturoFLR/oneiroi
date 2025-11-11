5be2c7e17d124dfe377afd84104173dc8b16558c
Arturo López <jhonas29@gmail.com>
2025-11-11 19:18:07 +0100

    feat(cinematics): new cinematics added to the "CinematicName" type in cinematicSlice.ts
    - added "alisedaParkFail01" and "nataliaHouseIntro01" cinematics

--- src/store/slices/cinematicSlice.ts
+++ src/store/slices/cinematicSlice.ts
@@ -3,10 +3,14 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 // Este slice controla qué cinemática será reproducida si el estado principal cambia a "cinematic"
 
 // Añadir aquí los nombres únicos de las cinemáticas que vayamos creando.
-export type CinematicName = "intro" | "alisedaPark01";
+export type CinematicName =
+  | "intro"
+  | "alisedaPark01"
+  | "alisedaParkFail01"
+  | "nataliaHouseIntro01";
 
 interface CinematicData {
-  cinematicToPlayName: CinematicName; // Qué cinemática se reproduce si cambiamos el estado generar a "cinematic"
+  cinematicToPlayName: CinematicName; // Qué cinemática se reproduce si cambiamos el estado generar a "cinematic". Nos permite buscarla en "allCinematicsData".
   isUserWatchingCinematics: boolean; // Se usa para saber si el usuario está viendo una cinemática desde el menú ppal, no desde el juego.
   //  De esta manera, al terminar la cinemática lo tenemos en cuenta para saber a qué parte de la app volver.
 }
