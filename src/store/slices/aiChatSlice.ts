f6570cabd1a9fd33720f7c217a977acd3a763c68
Arturo López <jhonas29@gmail.com>
2025-11-11 19:15:59 +0100

    chore(aiChat): NPC name "Natalia" modified to "natalia" in aiChatSlice.ts

--- src/store/slices/aiChatSlice.ts
+++ src/store/slices/aiChatSlice.ts
@@ -2,14 +2,14 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 // Este slice controla los datos del chat de IA, como el interlocutor actual.
 
-export type NPCName = "Natalia"; // Añadir aqui los nombres de los NPCs que vayamos creando.
+export type NPCName = "natalia"; // Añadir aqui los nombres de los NPCs que vayamos creando.
 
 interface AIChatData {
   currentNPCName: NPCName; // Nombre del NPC con el que se está interactuando
 }
 
 const initialState: AIChatData = {
-  currentNPCName: "Natalia", // Nombre del primer NPC cone el que se interactúa
+  currentNPCName: "natalia", // Nombre del primer NPC cone el que se interactúa. Se usa para buscar en "allNPCsData" los datos del NPC actual.
 };
 
 const aiChatSlice = createSlice({
