38e10578c3134b7baf49332a6d770c327b861b54
Arturo López <jhonas29@gmail.com>
2025-11-27 12:12:39 +0100

    feat(scenario): add getMainCell utility function
    - Used to retrieve the main cell id of a group in the map.

---
+++ src/utils/scenario/getMainCell.ts
@@ -0,0 +1,31 @@
+// Esta función comprueba si la celda pasada como parámetro (id) forma parte de un grupo.
+// Si es así, devuelve la celda principal del grupo. Si no, devuelve la misma celda.
+
+import Scenario from "../../classes/scenarios/Scenario";
+import MapCell from "../..//classes/map/MapCell";
+
+export default function getMainCell(scenario: Scenario, cellId: number) {
+  const cell: MapCell | undefined = scenario.map.find(
+    (mapCell) => mapCell.id === cellId
+  );
+
+  if (!cell) {
+    console.log(
+      `Error en getMainCell: La celda con id ${cellId} no existe en el mapa del escenario ${scenario.name}.`
+    );
+    return null;
+  }
+
+  if (cell.group && cell.group !== "void") {
+    if (!cell.mainGroupCellId) {
+      console.log(
+        `Error en getMainCell: La celda con id ${cellId} pertenece al grupo "${cell.group}", pero no tiene definido mainGroupCellId.`
+      );
+      return null;
+    } else {
+      return cell.mainGroupCellId;
+    }
+  } else {
+    return cellId;
+  }
+}
