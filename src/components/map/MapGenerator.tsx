9eda392f6fb7438178863c8f0ada1abd3b5a1e8e
Arturo López <jhonas29@gmail.com>
2025-12-04 10:38:55 +0100

    feat(map): add ScreenFader component and several new props to MapGenerator
    - ScreenFader adds a fadein and fadeout effect to the map
    - new props: actualCellData, visible, fadeDuration, onClickOutsideMap, onCellClick
    - theese props are used to add the fade effect and to make the map cells interactive

--- src/components/map/MapGenerator.tsx
+++ src/components/map/MapGenerator.tsx
@@ -5,12 +5,25 @@ import MapCell from "../../classes/map/MapCell";
 import MapCellVoid from "./styled/MapCellVoid";
 import MapCellRoom from "./styled/MapCellRoom";
 import MapContainer from "./styled/MapContainer";
+import ScreenFader from "../common/ScreenFader";
 
 type MapGeneratorProps = {
+  actualCellData: MapCell;
   mapCells: MapCell[];
+  visible: boolean;
+  fadeDuration: number;
+  onClickOutsideMap: (e: React.MouseEvent<HTMLElement>) => void;
+  onCellClick: (e: React.MouseEvent<HTMLElement>) => void;
 };
 
-function MapGenerator({ mapCells }: MapGeneratorProps) {
+function MapGenerator({
+  actualCellData,
+  mapCells,
+  visible,
+  fadeDuration,
+  onClickOutsideMap,
+  onCellClick,
+}: MapGeneratorProps) {
   const colsNumber = getMaxColsRows(mapCells, "col");
 
   function generateMapCells(): React.JSX.Element[] {
@@ -18,14 +31,21 @@ function MapGenerator({ mapCells }: MapGeneratorProps) {
 
     mapCells.forEach((mapCell) => {
       if (mapCell.group === "void" || mapCell.hidden === true) {
-        result.push(<MapCellVoid key={mapCell.id} />);
+        result.push(
+          <MapCellVoid key={mapCell.id} id={mapCell.id.toString()} />
+        );
         return;
       } else {
         const cellBorders = generateCellBorders(mapCell, mapCells);
+        const isCellClickable = actualCellData.reachableCells?.includes(
+          mapCell.id
+        );
 
         result.push(
           <MapCellRoom
             key={mapCell.id}
+            id={mapCell.id.toString()}
+            clickable={isCellClickable === true}
             doorsToShow={mapCell.doorsToShow}
             bordersToShow={cellBorders}
             name={mapCell.name}
@@ -33,6 +53,7 @@ function MapGenerator({ mapCells }: MapGeneratorProps) {
             hasPuzzle={mapCell.hasPuzzle}
             hasNpc={mapCell.hasNpc}
             hasSpirit={mapCell.hasSpirit}
+            onClick={onCellClick}
           />
         );
       }
@@ -41,7 +62,18 @@ function MapGenerator({ mapCells }: MapGeneratorProps) {
     return result;
   }
 
-  return <MapContainer $cols={colsNumber}>{generateMapCells()}</MapContainer>;
+  return (
+    <ScreenFader
+      visible={visible}
+      fadeDuration={fadeDuration}
+      color="light"
+      zIndex={50}
+      flex={true}
+      onClick={(e: React.MouseEvent<HTMLElement>) => onClickOutsideMap(e)}
+    >
+      <MapContainer $cols={colsNumber}>{generateMapCells()}</MapContainer>;
+    </ScreenFader>
+  );
 }
 
 export default MapGenerator;
