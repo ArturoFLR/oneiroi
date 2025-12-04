c6a5f930801240b2983d2c7992c429403cd463b8
Arturo López <jhonas29@gmail.com>
2025-12-04 10:51:59 +0100

    fix(npcs): change npc name "natalia" to "Natalia" in allNPCsData

--- src/data/npcs/allNPCsData.ts
+++ src/data/npcs/allNPCsData.ts
@@ -2,8 +2,9 @@ import { NPCName } from "../../store/slices/aiChatSlice";
 import NPC from "../../classes/npcs/NPC";
 import nataliaConfig from "./natalia/nataliaConfig";
 
+// Usamos comillas en los nombres de las propiedades para poder usar nombres complejos como "Ana María", que llevan espacios o tildes
 const allNPCsData: Record<NPCName, NPC> = {
-  natalia: new NPC(nataliaConfig),
+  Natalia: new NPC(nataliaConfig),
 };
 
 export default allNPCsData;
